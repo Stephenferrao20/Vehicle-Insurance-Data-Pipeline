8024b4301820e9fc4837dd0fd42b285a54834de8
Stephenferrao20 <stephenferrao20.in@gmail.com>
2025-06-08 16:26:09 +0530

    project structure setuped and initiated the virtual environment

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Stephen Ferrao",
+    author_email="stephenferrao20@gmail.com",
+    packages=find_packages()
+)
